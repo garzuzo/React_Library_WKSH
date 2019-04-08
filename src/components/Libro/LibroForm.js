f8d229694a332197f3504a8a6d040f62b9189f1e
mishale22 <sami_na11@hotmail.com>
2019-04-07 22:03:44 -0500

    changes in LibroForm

--- src/components/Libro/LibroForm.js
+++ src/components/Libro/LibroForm.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
-
+import firebase from '../../config/Firebase';
 class LibroForm extends Component {
 
   constructor (){
     super();
     this.state = {
-         id: '',  name: '', author: '', ISBN:'', genre:'', description:'', library:''
+         id: '',  name: '', author: '', isbn:'', genre:'', description:'', library:'',
+         libros:[], db: firebase.firestore()
     };
     
   }
 
-  componentWillReceiveProps(props){
-    this.setState({
-      id: props.libroEdit.id,
-      name: props.libroEdit.name,
-      author: props.libroEdit.author      
-    });
-  }
+ // componentWillReceiveProps(props){
+   // this.setState({
+     // id: props.libroEdit.id,
+      //name: props.libroEdit.name,
+      //author: props.libroEdit.author      
+   // });
+ // }
+
+ componentDidMount(){
+  this.state.db.collection("libros").get().then((querySnapshot)=>
+      querySnapshot.forEach(doc =>{
+        console.log(doc.data());
+        let auxLibros = this.state.libros;
+        auxLibros.push(doc.data());
+        this.setState( {libros: auxLibros});
+      })
+  );
+}
 
   handleSubmit(e){
       e.preventDefault();
@@ -28,12 +40,36 @@ class LibroForm extends Component {
       let libro = {
         id: this.state.id,
         name: this.state.name,
-        author: this.state.author
+        author: this.state.author,
+        genre: this.state.genre,
+        description: this.state.description,
+        isbn:this.state.isbn,
+        library: this.state.library
       }
 
-      this.props.handleNuevoLibro(libro);
+     // this.props.handleNuevoLibro(libro);
+     this.createBook(libro);
 
   }
+
+  createBook(libro){
+
+    let  libros = this.state.libros;
+
+    if(libro.id!==""){
+      console.log("Este libro ya existe");
+    }
+    else{
+      let  id  = Math.floor(Math.random()*100000)+1 + "";
+      libro.id = id;
+      libros.push(libro);
+    }
+    this.setState({
+      libros: libros
+    });
+    //console.log(libros);
+
+}
 
   handleOnChange (event){
    // console.log(event.target.value);
@@ -43,35 +79,35 @@ class LibroForm extends Component {
     });
   }
 
-  
+ 
   render() { 
     return (
-      <div className="LibroForm">
+      <div className="LibroForm container mt-5">
         <form onSubmit={this.handleSubmit.bind(this)}>
             <input type="hidden" value={this.state.id} onChange={this.handleOnChange.bind(this)} />
             <div className="form-group">
                 <label htmlFor="isbn">ISBN</label>
-                <input type="text" className="form-control" id="isbn" name="isbn" value={this.state.ISBN} />
+                <input type="text" className="form-control" id="isbn" name="isbn" onChange={this.handleOnChange.bind(this)} value={this.state.isbn} />
             </div>
             <div className="form-group">
                 <label htmlFor="name">Nombre</label>
-                <input type="text" className="form-control" id="name" name="name" value={this.state.name}  />
+                <input type="text" className="form-control" id="name" name="name" onChange={this.handleOnChange.bind(this)} value={this.state.name}  />
             </div>
             <div className="form-group">
                 <label htmlFor="author">Autor</label>
-                <input type="text" className="form-control" id="author" name="author" value={this.state.author} />
+                <input type="text" className="form-control" id="author" name="author" onChange={this.handleOnChange.bind(this)} value={this.state.author} />
             </div>
             <div className="form-group">
                 <label htmlFor="genre">Género</label>
-                <input type="text" className="form-control" id="genre" name="genre" value={this.state.genre} />
+                <input type="text" className="form-control" id="genre" name="genre" onChange={this.handleOnChange.bind(this)} value={this.state.genre} />
             </div>
             <div className="form-group">
                 <label htmlFor="description">Descripción</label>
-                <input type="text" className="form-control" id="description" name="description" value={this.state.description} />
+                <input type="text" className="form-control" id="description" name="description" onChange={this.handleOnChange.bind(this)} value={this.state.description} />
             </div>
             <div className="form-group">
                 <label htmlFor="library">Libreria</label>
-                <select class="form-control" id="description"  name="library" value={this.state.library}>
+                <select className="form-control" id="description"  name="library" onChange={this.handleOnChange.bind(this)}>
                     <option>Libreria de Leonardo</option>
                     <option>Libreria de Sami</option>
                     <option>Libreria de Johna</option>
