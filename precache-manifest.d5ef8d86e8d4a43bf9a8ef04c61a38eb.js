0bb55e716b724999e7df7fe21d35882d9a0e6c10
garzuzo <garzuxo@gmail.com>
2019-04-09 09:57:49 -0500

    Updates

---
+++ precache-manifest.d5ef8d86e8d4a43bf9a8ef04c61a38eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97242a6fb75e626797fa",
+    "url": "/React_Library_WKSH/static/js/runtime~main.6afea1da.js"
+  },
+  {
+    "revision": "f9086137eff8c241c8bf",
+    "url": "/React_Library_WKSH/static/js/main.332fceb9.chunk.js"
+  },
+  {
+    "revision": "abddc423e042112f732e",
+    "url": "/React_Library_WKSH/static/js/2.42945c74.chunk.js"
+  },
+  {
+    "revision": "f9086137eff8c241c8bf",
+    "url": "/React_Library_WKSH/static/css/main.a264b511.chunk.css"
+  },
+  {
+    "revision": "30a43de0049ed8118fd79c31125d3db4",
+    "url": "/React_Library_WKSH/index.html"
+  }
+];
